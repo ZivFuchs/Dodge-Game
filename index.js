9fde1ee4a0cd425fea4af3cdf9d46a167510b71f
Ziv Fuchs <ziv.fuchs@gmail.com>
2021-12-16 17:31:58 -0800

    updated comments

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const framesPerSecond = 60;
 
+// declare constants
 let GAME_DIMENSION = 1000;
 let PLAYER_SIZE = GAME_DIMENSION * 0.05;
 let PLAYER_VELOCITY = PLAYER_SIZE * 15;
 let PROJECTILE_SIZE = PLAYER_SIZE * 0.3;
 let PROJECTILE_FREQUENCY = 1;
 
+// initialize list of projectiles to be empty, points to be 0
 let projectiles = [];
 let points = 0;
 
 class Player {
     constructor() {
-        // center player
+        // place player in the center of the battlefield
         this.x = (GAME_DIMENSION / 2) - (PLAYER_SIZE / 2);
         this.y = this.x;
 
@@ -38,23 +40,25 @@ class Player {
         
         // check each projectile for collission
         for (let i = 0; i < projectiles.length; i++) {
-            if ((projectiles[i].x >= player.x && projectiles[i].x <= player.x + PLAYER_SIZE) && (projectiles[i].y >= player.y && projectiles[i].y <= player.y + PLAYER_SIZE)) {
+            if ((projectiles[i].x + projectiles[i].size >= player.x && projectiles[i].x <= player.x + PLAYER_SIZE) && (projectiles[i].y + projectiles[i].size >= player.y && projectiles[i].y <= player.y + PLAYER_SIZE)) {
                 gameOver = true;
             }
         }
     }
 
     draw() {
+        // draw the player as a black square
         ctx.fillStyle = "black";
         ctx.fillRect(player.x, player.y, PLAYER_SIZE, PLAYER_SIZE);
     }
 }
 
-
-
 class Projectile {
     constructor(player) {
+        // randomly assign each projectile a size
         this.size = PROJECTILE_SIZE * randomNumberFromRange(0.5, 1.5);
+
+        // randomly place each projectile outside of the game screen
         switch(Math.floor(randomNumberFromRange(0,3))) {
             // above battlefield
             case 0:
@@ -80,9 +84,13 @@ class Projectile {
 
         // let timeUntilPlayer = (randomNumberFromRange(50, 100));
         // let timeUntilPlayer = 100;
+
+        // randomly assigne ach projectile a speed
         let timeUntilPlayer = randomNumberFromRange(1, 2);
         this.dx = (player.x - this.x) / timeUntilPlayer;
         this.dy = (player.y - this.y) / timeUntilPlayer;
+
+        // don't check for deletion at first, only start checking once projectile has entered the battlefield
         this.checkDeletion = false;
     }
 
@@ -92,18 +100,35 @@ class Projectile {
         this.y += this.dy * deltaTime;
         // check for collision?
 
+        // if the projectile has entered the battlefield, start checking it for deletion  
         if (this.x < GAME_DIMENSION && this.x > 0 && this.y <GAME_DIMENSION && this.y > 0) {
             this.checkDeletion = true
         }
     }
 
     draw() {
-        // change fillstyle to appropriate
+        // change fillstyle to red
         ctx.fillStyle = "red";
-        // draw
+        // draw the projectile
         ctx.fillRect(this.x, this.y, this.size, this.size);
     }
 }
+
+// async function test() {
+//     console.log("waiting for press");
+//     await waitingKeyPress();
+//     console.log('good job!');
+// }
+
+// function waitingKeyPres() {
+//     return new Promise((resolve) => {
+//         document.addEventListener('keydown', onKeyHandler);
+//         function onKeyHandler(e) {
+//             document.removeEventListener('keydown', onKeyHandler);
+//             resolve();
+//         }
+//     });
+// }
 
 let player = new Player(GAME_DIMENSION, PLAYER_SIZE, PLAYER_VELOCITY);
 
@@ -115,6 +140,7 @@ canvas.height = GAME_DIMENSION;
 // this array is used to manage which direction the player is moving
 let held_directions = [];
 
+// this keydown event listener is used to control the player
 document.addEventListener('keydown', (e) => {
     switch(e.code)
     {
@@ -210,6 +236,7 @@ function main(currentTime) {
         }
     }
 
+    // loop the character around the battlefield
     if (player.x + PLAYER_SIZE < 0) player.x = GAME_DIMENSION;
     if (player.x > GAME_DIMENSION )player.x = 0 - PLAYER_SIZE;
     if (player.y + PLAYER_SIZE < 0) player.y = GAME_DIMENSION;
@@ -230,20 +257,25 @@ function main(currentTime) {
     PROJECTILE_FREQUENCY += 0.001 * deltaTime;
     PROJECTILE_SIZE += 0.001 * deltaTime;
 
+    // if the player dies, restart the game
     if (gameOver) {
-        if (confirm("Press any key to continue")) {
-            // restart the game
-            gameOver = false;
-            let PROJECTILE_SIZE = PLAYER_SIZE * 0.3;
-            let PROJECTILE_FREQUENCY = 1;
+        gameOver = false;
+        // reset projectile size and frequency
+        let PROJECTILE_SIZE = PLAYER_SIZE * 0.3;
+        PROJECTILE_FREQUENCY = 1;
 
-            projectiles = [];
-            
-            this.x = (GAME_DIMENSION / 2) - (PLAYER_SIZE / 2);
-            this.y = this.x;
-        }
+        // reset projectiles and held_directions arrays
+        projectiles = [];
+        held_directions = [];
+        
+        // recenter the player
+        player.x = (GAME_DIMENSION / 2) - (PLAYER_SIZE / 2);
+        player.y = player.x;
+        console.log(points);
+
+        // reset points to 0
+        points = 0;
     }
-
 }
 
 window.requestAnimationFrame(main);
